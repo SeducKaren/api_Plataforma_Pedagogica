9f65664ae267614ce58dc87d243b67db7f7083b4
Mylena Oliveira <139914402+mylenaaraujo08@users.noreply.github.com>
2024-04-07 14:53:31 -0300

    Update gestorRoutes.ts

--- src/routes/gestorRoutes.ts
+++ src/routes/gestorRoutes.ts
@@ -1,47 +1,38 @@
-// gestorRoutes.ts
-import express, { Request, Response } from 'express';
+import express from 'express';
 import GestorController from '../controllers/gestorController';
 
 const router = express.Router();
 
-// Get all gestores
-router.get('/', async (req: Request, res: Response) => {
-    await GestorController.getAllGestores(req, res);
+router.get('/', async (req, res) => {
+  await GestorController.getAllGestores(req, res);
 });
 
-// Get gestor by ID
-router.get('/:id', async (req: Request, res: Response) => {
-    await GestorController.getGestorById(req, res);
+router.get('/:id', async (req, res) => {
+  await GestorController.getGestorById(req, res);
 });
 
-// Get gestor by CPF
-router.get('/cpf/:cpf', async (req: Request, res: Response) => {
-    await GestorController.getGestorByCpf(req, res);
+router.get('/cpf/:cpf', async (req, res) => {
+  await GestorController.getGestorByCpf(req, res);
 });
 
-// Get gestor by nome
-router.get('/nome/:nome', async (req: Request, res: Response) => {
-    await GestorController.getGestorByNome(req, res);
+router.get('/nome/:nome', async (req, res) => {
+  await GestorController.getGestorByNome(req, res);
 });
 
-// Get gestor by escola
-router.get('/escola/:escola', async (req: Request, res: Response) => {
-    await GestorController.getGestorByEscola(req, res);
+router.get('/escola/:escola', async (req, res) => {
+  await GestorController.getGestorByEscola(req, res);
 });
 
-// Create new gestor
-router.post('/', async (req: Request, res: Response) => {
-    await GestorController.createGestor(req, res);
+router.post('/', async (req, res) => {
+  await GestorController.createGestor(req, res);
 });
 
-// Update gestor by ID
-router.put('/:id', async (req: Request, res: Response) => {
-    await GestorController.updateGestor(req, res);
+router.put('/:id', async (req, res) => {
+  await GestorController.updateGestor(req, res);
 });
 
-// Delete gestor by ID
-router.delete('/:id', async (req: Request, res: Response) => {
-    await GestorController.deleteGestor(req, res);
+router.delete('/:id', async (req, res) => {
+  await GestorController.deleteGestor(req, res);
 });
 
 export default router;
