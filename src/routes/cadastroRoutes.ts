a5cd2e99b765bf67156d2dfdb252e01b9bf13086
Mylena Oliveira <139914402+mylenaaraujo08@users.noreply.github.com>
2024-04-07 13:18:48 -0300

    Update cadastroRoutes.ts

--- src/routes/cadastroRoutes.ts
+++ src/routes/cadastroRoutes.ts
@@ -3,14 +3,17 @@ import CadastroController from "../controllers/cadastroController";
 
 const router = express.Router();
 
+// Rota para buscar usuários por nome da escola
 router.get("/escola/:nomeEscola?", async (req: Request, res: Response) => {
   let { nomeEscola } = req.params;
 
+  // Se nenhum nome de escola for fornecido, deixe-o como uma string vazia para buscar todas as escolas
   if (!nomeEscola) {
     nomeEscola = "";
   }
 
   try {
+    // Chamar o método correto do CadastroController
     await CadastroController.getUsuariosByEscola(req, res);
   } catch (error) {
     console.error(error);
@@ -18,10 +21,12 @@ router.get("/escola/:nomeEscola?", async (req: Request, res: Response) => {
   }
 });
 
+// Rota para buscar usuários por nome
 router.get("/nome/:nome", async (req: Request, res: Response) => {
   const { nome } = req.params;
 
   try {
+    // Chamar o método correto do CadastroController
     await CadastroController.getUsuariosByNome(req, res);
   } catch (error) {
     console.error(error);
@@ -29,10 +34,12 @@ router.get("/nome/:nome", async (req: Request, res: Response) => {
   }
 });
 
+// Rota para buscar usuários por CPF
 router.get("/cpf/:cpf", async (req: Request, res: Response) => {
   const { cpf } = req.params;
 
   try {
+    // Chamar o método correto do CadastroController
     await CadastroController.getUsuariosByCPF(req, res);
   } catch (error) {
     console.error(error);
@@ -40,20 +47,11 @@ router.get("/cpf/:cpf", async (req: Request, res: Response) => {
   }
 });
 
+// Rota para buscar todos os usuários
 router.get("/", async (req: Request, res: Response) => {
   try {
     // Chamar o método correto do CadastroController
     await CadastroController.getAllUsuarios(req, res);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-});
-
-router.post("/", async (req: Request, res: Response) => {
-  try {
-    const newUser = await CadastroController.cadastrarUsuario(req, res);
-    res.status(201).json(newUser);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
