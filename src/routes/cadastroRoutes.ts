83bb32df06f2a91a695ef1652c585755c279470f
Seduc_Plataforma_GPS <166050747+SeducKaren@users.noreply.github.com>
2024-04-07 13:00:41 -0300

    Update cadastroRoutes.ts

--- src/routes/cadastroRoutes.ts
+++ src/routes/cadastroRoutes.ts
@@ -3,17 +3,14 @@ import CadastroController from "../controllers/cadastroController";
 
 const router = express.Router();
 
-// Rota para buscar usuários por nome da escola
 router.get("/escola/:nomeEscola?", async (req: Request, res: Response) => {
   let { nomeEscola } = req.params;
 
-  // Se nenhum nome de escola for fornecido, deixe-o como uma string vazia para buscar todas as escolas
   if (!nomeEscola) {
     nomeEscola = "";
   }
 
   try {
-    // Chamar o método correto do CadastroController
     await CadastroController.getUsuariosByEscola(req, res);
   } catch (error) {
     console.error(error);
@@ -21,12 +18,10 @@ router.get("/escola/:nomeEscola?", async (req: Request, res: Response) => {
   }
 });
 
-// Rota para buscar usuários por nome
 router.get("/nome/:nome", async (req: Request, res: Response) => {
   const { nome } = req.params;
 
   try {
-    // Chamar o método correto do CadastroController
     await CadastroController.getUsuariosByNome(req, res);
   } catch (error) {
     console.error(error);
@@ -34,12 +29,10 @@ router.get("/nome/:nome", async (req: Request, res: Response) => {
   }
 });
 
-// Rota para buscar usuários por CPF
 router.get("/cpf/:cpf", async (req: Request, res: Response) => {
   const { cpf } = req.params;
 
   try {
-    // Chamar o método correto do CadastroController
     await CadastroController.getUsuariosByCPF(req, res);
   } catch (error) {
     console.error(error);
@@ -47,7 +40,6 @@ router.get("/cpf/:cpf", async (req: Request, res: Response) => {
   }
 });
 
-// Rota para buscar todos os usuários
 router.get("/", async (req: Request, res: Response) => {
   try {
     // Chamar o método correto do CadastroController
@@ -58,7 +50,6 @@ router.get("/", async (req: Request, res: Response) => {
   }
 });
 
-// Rota para cadastrar um novo usuário
 router.post("/", async (req: Request, res: Response) => {
   try {
     const newUser = await CadastroController.cadastrarUsuario(req, res);
