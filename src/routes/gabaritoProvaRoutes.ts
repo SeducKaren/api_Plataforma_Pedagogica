a019067c6d7bc01aeb7522fd0a0e7d4f02f52bdc
Vinicius Oliveira <vinicius.dsro@hotmail.com>
2024-04-22 05:15:09 -0300

    Atualizando a branch e adicinando o gabarito

---
+++ src/routes/gabaritoProvaRoutes.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import { GabaritoProvasController } from '../controllers/gabaritoProvasController';
+
+const router = express.Router();
+
+router.get('/', GabaritoProvasController.getAll);
+router.get('/:nivel', GabaritoProvasController.getByNivel);
+router.post('/', GabaritoProvasController.create);
+router.put('/:nivel', GabaritoProvasController.updateByNivel);
+router.delete('/:nivel', GabaritoProvasController.deleteByNivel);
+
+export default router;
+
