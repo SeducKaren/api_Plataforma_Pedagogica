7e92c18029d174a4b2ae28ef39bd0fed4675f588
Mylena Oliveira <139914402+mylenaaraujo08@users.noreply.github.com>
2024-04-24 01:36:21 -0300

    Update indexRoutes.ts

--- src/routes/indexRoutes.ts
+++ src/routes/indexRoutes.ts
@@ -3,7 +3,8 @@ import escolaRouter from './escolaRoutes';
 import turmaRouter from './turmaRoutes';
 import alunoRouter from './alunoRoutes';
 import gestorRouter from './gestorRoutes';
-import cadastroRouter from './cadastroRoutes'; 
+import cadastroRouter from './cadastroRoutes';
+import resultadosProvasRouter from './resultadosProvasRoutes';
 
 const indexRouter = express.Router();
 
@@ -11,6 +12,7 @@ indexRouter.use('/escola', escolaRouter);
 indexRouter.use('/turma', turmaRouter);
 indexRouter.use('/aluno', alunoRouter);
 indexRouter.use('/gestor', gestorRouter);
-indexRouter.use('/cadastro', cadastroRouter); 
+indexRouter.use('/cadastro', cadastroRouter);
+indexRouter.use('/resultados-provas', resultadosProvasRouter);
 
 export default indexRouter;
