8039f49148a09a6d17a9f1c422904ece99bc72a9
Seduc_Plataforma_GPS <166050747+SeducKaren@users.noreply.github.com>
2024-04-27 14:51:54 -0300

    Update escolaRoutes.ts

--- src/routes/escolaRoutes.ts
+++ src/routes/escolaRoutes.ts
@@ -18,7 +18,7 @@ router.post("/", async (req: Request, res: Response) => {
   try {
     // Chame o método save de EscolaController para criar uma nova escola
     const novaEscola = await EscolaController.save(req, res);
-    res.status(201).json(novaEscola); // Retorne a nova escola criada
+    res.status(201).json(novaEscola);
   } catch (error) {
     console.error("Erro ao salvar nova escola:", error);
     res.status(500).json({ message: "Erro interno do servidor ao criar a escola." });
