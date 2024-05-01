458f2040577365cd13eaeaf3ed4b257d3862f7d7
Mylena Oliveira <139914402+mylenaaraujo08@users.noreply.github.com>
2024-05-01 19:09:24 -0300

    Update indexControllers.ts

--- src/controllers/indexControllers.ts
+++ src/controllers/indexControllers.ts
@@ -1,9 +1,8 @@
 import AlunoController from "./alunoController";
-import CadastroController from "./cadastroController"; 
 import EscolaController from "./escolaController";
-import { GabaritoProvasController } from "./gabaritoProvasController";
 import GestorController from "./gestorController";
 import TurmaController from "./turmaController";
+import CadastroController from "./cadastroController";
 import ResultadosProvasController from "./resultadoProvasController";
 
-export { AlunoController, CadastroController, EscolaController, GabaritoProvasController, GestorController, TurmaController, ResultadosProvasController }; 
+export { AlunoController, EscolaController, GestorController, TurmaController, CadastroController, ResultadosProvasController };
