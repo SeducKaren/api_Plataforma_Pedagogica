f541233ca0f7222d323b23972c70e9a99e07e70e
Mylena Oliveira <139914402+mylenaaraujo08@users.noreply.github.com>
2024-05-01 18:58:54 -0300

    Update and rename resultadosProvasRoutes.ts to resultadoProvasRoutes.ts

---
+++ src/routes/resultadoProvasRoutes.ts
@@ -0,0 +1,9 @@
+import AlunoModel from "./alunoModel";
+import CadastroModel from "./cadastroModel";
+import EscolaModel from "./escolaModel";
+import GabaritoProvasModel from "./gabaritoProvasModel";
+import GestorModel from "./gestorModel";
+import ResultadosProvasModel from "./resultadoProvasModel";
+import TurmaModel from "./turmaModel";
+
+export { AlunoModel, CadastroModel, EscolaModel, GabaritoProvasModel, GestorModel, ResultadosProvasModel, TurmaModel};
