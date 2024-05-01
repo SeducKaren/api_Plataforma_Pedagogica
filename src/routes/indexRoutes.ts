1c6928d37f5f170077a8b8ab929314e38816bc22
Mylena Oliveira <139914402+mylenaaraujo08@users.noreply.github.com>
2024-05-01 19:18:10 -0300

    Update indexRoutes.ts

--- src/routes/indexRoutes.ts
+++ src/routes/indexRoutes.ts
@@ -1,20 +1,18 @@
 import express from 'express';
-import alunoRouter from './alunoRoutes';
-import cadastroRouter from './cadastroRoutes'; 
 import escolaRouter from './escolaRoutes';
-import gabaritoProvasRouter from './gabaritoProvaRoutes';
-import gestorRouter from './gestorRoutes';
-import resultadosProvasRouter from './resultadoProvasRoutes';
 import turmaRouter from './turmaRoutes';
+import alunoRouter from './alunoRoutes';
+import gestorRouter from './gestorRoutes';
+import cadastroRouter from './cadastroRoutes';
+import resultadosProvasRouter from './resultadoProvasRoutes';
 
 const indexRouter = express.Router();
 
-indexRouter.use('/aluno', alunoRouter);
-indexRouter.use('/cadastro', cadastroRouter); 
 indexRouter.use('/escola', escolaRouter);
-indexRouter.use('gabaritoProvasRouter',gabaritoProvasRouter); 
-indexRouter.use('/gestor', gestorRouter);
-indexRouter.use('/resultadoProvas', resultadosProvasRouter)
 indexRouter.use('/turma', turmaRouter);
+indexRouter.use('/aluno', alunoRouter);
+indexRouter.use('/gestor', gestorRouter);
+indexRouter.use('/cadastro', cadastroRouter);
+indexRouter.use('/resultados-provas', resultadosProvasRouter);
 
 export default indexRouter;
