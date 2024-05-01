ccdccddd7aa0d95d4ea0b8cc78b13c9daf83cd8a
Mylena Oliveira <139914402+mylenaaraujo08@users.noreply.github.com>
2024-05-01 20:30:30 -0300

    Update resultadoProvasController.ts

--- src/controllers/resultadoProvasController.ts
+++ src/controllers/resultadoProvasController.ts
@@ -2,6 +2,22 @@ import { Request, Response } from "express";
 import ResultadosProvasModel from "../models/resultadoProvasModel";
 
 class ResultadosProvasController {
+  static async getProvasById(req: Request, res: Response): Promise<void> {
+    const id = parseInt(req.params.id);
+
+    try {
+      const prova = await ResultadosProvasModel.findById(id);
+      if (prova) {
+        res.status(200).json(prova);
+      } else {
+        res.status(404).json({ message: "Prova não encontrada" });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+
   static async getProvasByEscola(req: Request, res: Response): Promise<void> {
     let nomeEscola = req.params.nomeEscola;
 
@@ -11,18 +27,6 @@ class ResultadosProvasController {
 
     try {
       const provas = await ResultadosProvasModel.findBySchoolName(nomeEscola);
-      res.status(200).json(provas);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: "Internal server error" });
-    }
-  }
-
-  static async getProvasByNome(req: Request, res: Response): Promise<void> {
-    const nome = req.params.nome;
-
-    try {
-      const provas = await ResultadosProvasModel.findByNome(nome);
       res.status(200).json(provas);
     } catch (error) {
       console.error(error);
@@ -42,10 +46,24 @@ class ResultadosProvasController {
     }
   }
 
-  static async getAllProvas(req: Request, res: Response): Promise<void> {
+  static async getProvasByNome(req: Request, res: Response): Promise<void> {
+    const nome = req.params.nome;
+
     try {
-      const provas = await ResultadosProvasModel.getAll();
+      const provas = await ResultadosProvasModel.findByNome(nome);
       res.status(200).json(provas);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  }
+
+  static async cadastrarProva(req: Request, res: Response): Promise<void> {
+    const provaData = req.body;
+
+    try {
+      const novaProva = await ResultadosProvasModel.create(provaData);
+      res.status(201).json(novaProva);
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: "Internal server error" });
