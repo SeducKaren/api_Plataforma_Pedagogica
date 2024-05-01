6eb24d9027b8a51fe360490e06cc4b192b755953
Mylena Oliveira <139914402+mylenaaraujo08@users.noreply.github.com>
2024-05-01 19:17:39 -0300

    Update indexModels.ts

--- src/models/indexModels.ts
+++ src/models/indexModels.ts
@@ -1,9 +1,8 @@
 import AlunoModel from "./alunoModel";
-import CadastroModel from "./cadastroModel";
 import EscolaModel from "./escolaModel";
-import GabaritoProvasModel from "./gabaritoProvasModel";
 import GestorModel from "./gestorModel";
-import ResultadosProvasModel from "./resultadoProvasModel";
 import TurmaModel from "./turmaModel";
+import CadastroModel from "./cadastroModel";
+import ResultadosProvasModel from "./resultadoProvasModel";
 
-export { AlunoModel, CadastroModel, EscolaModel, GabaritoProvasModel, GestorModel, ResultadosProvasModel, TurmaModel};
+export { AlunoModel, EscolaModel, GestorModel, TurmaModel, CadastroModel, ResultadosProvasModel };
