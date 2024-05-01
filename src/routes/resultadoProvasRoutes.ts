1672f6dcb420261fb0ba86426eec726f1ebfbb06
Mylena Oliveira <139914402+mylenaaraujo08@users.noreply.github.com>
2024-05-01 19:18:27 -0300

    Update resultadoProvasRoutes.ts

--- src/routes/resultadoProvasRoutes.ts
+++ src/routes/resultadoProvasRoutes.ts
@@ -1,9 +1,61 @@
-import AlunoModel from "./alunoModel";
-import CadastroModel from "./cadastroModel";
-import EscolaModel from "./escolaModel";
-import GabaritoProvasModel from "./gabaritoProvasModel";
-import GestorModel from "./gestorModel";
-import ResultadosProvasModel from "./resultadoProvasModel";
-import TurmaModel from "./turmaModel";
+import express, { Request, Response } from "express";
+import ResultadosProvasController from "../controllers/resultadoProvasController";
 
-export { AlunoModel, CadastroModel, EscolaModel, GabaritoProvasModel, GestorModel, ResultadosProvasModel, TurmaModel};
+const router = express.Router();
+
+router.get("/:id", async (req: Request, res: Response) => {
+  try {
+    await ResultadosProvasController.getProvasById(req, res);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+router.get("/escola/:nomeEscola?", async (req: Request, res: Response) => {
+  let { nomeEscola } = req.params;
+
+  if (!nomeEscola) {
+    nomeEscola = "";
+  }
+
+  try {
+    await ResultadosProvasController.getProvasByEscola(req, res);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+router.get("/matricula/:matricula", async (req: Request, res: Response) => {
+  const { matricula } = req.params;
+
+  try {
+    await ResultadosProvasController.getProvasByMatricula(req, res);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+router.get("/nome/:nome", async (req: Request, res: Response) => {
+  const { nome } = req.params;
+
+  try {
+    await ResultadosProvasController.getProvasByNome(req, res);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+router.post("/", async (req: Request, res: Response) => {
+  try {
+    await ResultadosProvasController.cadastrarProva(req, res);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+export default router;
